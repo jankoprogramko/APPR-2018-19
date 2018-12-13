33f7b12ba85d420eac5d85ee2089b6acf588fb0f
Jan Črne <jan.chrne@gmail.com>
2018-12-13 16:53:52 +0100

    a dela doma

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,31 +1,31 @@
 # 2. faza: Uvoz podatkov
 
-sl <- locale("sl", decimal_mark=",", grouping_mark=".")
+#sl <- locale("sl", decimal_mark=",", grouping_mark=".")
 
 # Funkcija, ki uvozi občine iz Wikipedije
-uvozi.obcine <- function() {
-  link <- "http://sl.wikipedia.org/wiki/Seznam_ob%C4%8Din_v_Sloveniji"
-  stran <- html_session(link) %>% read_html()
-  tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
-    .[[1]] %>% html_table(dec=",")
-  for (i in 1:ncol(tabela)) {
-    if (is.character(tabela[[i]])) {
-      Encoding(tabela[[i]]) <- "UTF-8"
-    }
-  }
-  colnames(tabela) <- c("obcina", "povrsina", "prebivalci", "gostota", "naselja",
-                        "ustanovitev", "pokrajina", "regija", "odcepitev")
-  tabela$obcina <- gsub("Slovenskih", "Slov.", tabela$obcina)
-  tabela$obcina[tabela$obcina == "Kanal ob Soči"] <- "Kanal"
-  tabela$obcina[tabela$obcina == "Loški potok"] <- "Loški Potok"
-  for (col in c("povrsina", "prebivalci", "gostota", "naselja", "ustanovitev")) {
-    tabela[[col]] <- parse_number(tabela[[col]], na="-", locale=sl)
-  }
-  for (col in c("obcina", "pokrajina", "regija")) {
-    tabela[[col]] <- factor(tabela[[col]])
-  }
-  return(tabela)
-}
+#uvozi.obcine <- function() {
+#  link <- "http://sl.wikipedia.org/wiki/Seznam_ob%C4%8Din_v_Sloveniji"
+#  stran <- html_session(link) %>% read_html()
+#  tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
+#    .[[1]] %>% html_table(dec=",")
+#  for (i in 1:ncol(tabela)) {
+#    if (is.character(tabela[[i]])) {
+#      Encoding(tabela[[i]]) <- "UTF-8"
+#    }
+#  }
+#  colnames(tabela) <- c("obcina", "povrsina", "prebivalci", "gostota", "naselja",
+#                        "ustanovitev", "pokrajina", "regija", "odcepitev")
+#  tabela$obcina <- gsub("Slovenskih", "Slov.", tabela$obcina)
+#  tabela$obcina[tabela$obcina == "Kanal ob Soči"] <- "Kanal"
+#  tabela$obcina[tabela$obcina == "Loški potok"] <- "Loški Potok"
+#  for (col in c("povrsina", "prebivalci", "gostota", "naselja", "ustanovitev")) {
+#    tabela[[col]] <- parse_number(tabela[[col]], na="-", locale=sl)
+#  }
+#  for (col in c("obcina", "pokrajina", "regija")) {
+#    tabela[[col]] <- factor(tabela[[col]])
+#  }
+#  return(tabela)
+#}
 
 data <- read_csv("https://raw.githubusercontent.com/H-Cox/FPL/master/alldata.csv",
                  locale=locale(encoding="UTF-8"))
@@ -34,6 +34,7 @@ igralci <- data[, 1:60]
 krogi <- data[, c(1, 61:ncol(data))] %>% melt(id.vars="id") %>%
   separate("variable", c("krog", "podatek"), ": ") %>%
   mutate(krog=parse_number(krog))
+#haha kok dobr
 
 # Zapišimo podatke v razpredelnico obcine
 #obcine <- uvozi.obcine()
